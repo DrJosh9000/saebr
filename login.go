2a4542b868c705470165046437f60f828958c666
Josh Deprez <DrJosh9000@users.noreply.github.com>
2020-11-26 11:08:42 +1100

    remove me.png ref from login

--- login.go
+++ login.go
@@ -29,7 +29,6 @@ var loginPageTmpl = template.Must(template.New("login.html").Parse(`<!DOCTYPE ht
 <head>
     <title>Login</title>
     <link rel="shortcut icon" href="/favicon.ico">
-    <link rel="icon" href="/static/me.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" media="screen,projection" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
